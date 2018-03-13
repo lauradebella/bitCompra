5564daab578f89b15e5fc472ccb94f2e7e823e83
lbella <lbella@thoughtworks.com>
2018-03-13 19:35:36 -0300

    Initial commit

---
+++ src/main/java/com/bitCompra/bitCompra/BitCompraApplication.java
@@ -0,0 +1,12 @@
+package com.bitCompra.bitCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitCompraApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bitCompra/bitCompra/BitCompraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bitCompra.bitCompra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BitCompraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
