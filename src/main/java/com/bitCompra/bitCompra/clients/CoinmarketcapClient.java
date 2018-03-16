1ceff40071a838a8a203dcbd708a2429994431b9
lbella <lbella@thoughtworks.com>
2018-03-16 09:04:54 -0300

    Add database icon

--- src/main/java/com/bitCompra/bitCompra/clients/CoinmarketcapClient.java
+++ src/main/java/com/bitCompra/bitCompra/clients/CoinmarketcapClient.java
@@ -1,6 +1,7 @@
 package com.bitCompra.bitCompra.clients;
 
 import com.bitCompra.bitCompra.models.CoinmarketcapResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ public class CoinmarketcapClient {
 
     String COIN_MARKET_URL = "https://api.coinmarketcap.com/v1/ticker/biztcoin/";
 
+    @Autowired
     RestTemplate restTemplate;
 
     public CoinmarketcapResponse getPrice() {
